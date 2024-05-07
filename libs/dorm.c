740ec01858ec50595ebe90f99430b9eeae0a350a
DinaSiagian <143679301+DinaSiagian@users.noreply.github.com>
2024-05-07 14:47:15 +0700

    pair 02

--- libs/dorm.c
+++ libs/dorm.c
@@ -11,5 +11,5 @@ struct dorm_t create_dorm(char *_name, unsigned short _capacity, enum gender_t _
   dorm.gender = _gender;
   dorm.residents_num = 0;
 
-  return dorm;
+  return dorm; 
 }
--- libs/repository.c
+++ libs/repository.c
@@ -3,17 +3,21 @@
 #include "student.h"
 #include "gender.h"
 #include <stdio.h>
+#include <stdlib.h> // Include the header file for exit()
+#include <errno.h> // Include the header file for stderr
 
 void repository_dorms(struct dorm_t *_dorms, int _num_dorms) {
-    char buff[255];
-    char text[255];
     FILE *fp;
 
     fp = fopen("dorms.txt", "w");
-    fp = fopen("students.txt", "w");
-
-    while(fgets(buff, sizeof(buff),fp)){
-        sscanf(buff, "%s", text);
+    if (fp == NULL) {
+        fprintf(stderr, "Failed to open dorms.txt for writing\n");
+        exit(1);
     }
+
+    for (int i = 0; i < _num_dorms; i++) {
+        fprintf(fp, "%s %d %s\n", _dorms[i].name, _dorms[i].capacity, _dorms[i].gender == GENDER_MALE ? "male" : "female");
+    }
+
     fclose(fp);
 }
